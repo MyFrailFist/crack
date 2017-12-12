b7d63be298bf82f76df4723dd91579e0e084a9db
MyFrailFist <lingngeehung@gmail.com>
2017-12-12 23:24:37 +0800

    pull

--- server/service/maintenanceService.js
+++ server/service/maintenanceService.js
@@ -17,8 +17,20 @@ exports.getAllMaintenanceReports = function(req, res){
 	})
 }
 
+exports.getOneMaintenanceReport = function(req, res){
+	var query = 'what is the query';
+
+	MaintenanceReports.findOne(query).exec(function(err, response){
+		if(err) return(err);
+		console.log(response);
+		return res.json(response);
+	})
+}
+
 
 exports.testMR = function(req, res){
+	//going to create some fake reports with this
 	console.log('owerwue');
+	var query = "wat is the query ";
 	return res.json();
 }
--- server/service/weatherService.js
+++ server/service/weatherService.js
@@ -22,3 +22,15 @@ exports.getWeather24hours = function(req, res){
 		return res.json(response)	
 	})
 }
+
+exports.getWeather7days = function(req, res){
+	var query = 'what is the query again';
+	console.log(query);
+	request({
+		url: 'ergergerg'
+	}, function(err, response){
+		if(err) return(err);
+		console.log(response);
+		return res.json(response)
+	})
+}
